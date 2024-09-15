39fe62999af2a7e42ab3d1637f6c5a72cb00bbc6
Ivan Fernandez FACEPHI <ivanfernandez@facephi.com>
2024-09-15 18:17:14 +0200

    feat: wip

--- src/Contexts/Mooc/Users/application/GenerateSuggestedUserCourseOnUserCourseFinished.ts
+++ src/Contexts/Mooc/Users/application/GenerateSuggestedUserCourseOnUserCourseFinished.ts
@@ -1,5 +1,7 @@
 import { DomainEventClass } from '../../../Shared/domain/eventBus/DomainEvent';
 import { DomainEventSubscriber } from '../../../Shared/domain/eventBus/DomainEventSubscriber';
+import { UuidValueObject } from '../../../Shared/domain/UuidValueObject';
+import { CourseCreator } from '../../Courses/application/CourseCreator';
 import { UserCourseFinishedDomainEvent } from '../domain/UserCourseFinishedDomainEvent';
 import { UserFinderDomainService } from '../domain/UserFinderDomainService';
 import { UserRepository } from '../domain/UserRepository';
@@ -9,7 +11,8 @@ export class GenerateSuggestedUserCourseOnUserCourseFinished implements DomainEv
 	constructor(
 		private readonly generator: UserSuggestedCourseGeneratorDomainService,
 		private readonly finderDomainService: UserFinderDomainService,
-		private readonly repository: UserRepository
+		private readonly repository: UserRepository,
+		private readonly courseCreator: CourseCreator
 	) {}
 
 	subscribedTo(): DomainEventClass[] {
@@ -19,10 +22,19 @@ export class GenerateSuggestedUserCourseOnUserCourseFinished implements DomainEv
 	async on(event: UserCourseFinishedDomainEvent): Promise<void> {
 		const user = await this.finderDomainService.run(event.aggregateId);
 
-		const newSuggestedCourseName = await this.generator.run([event.courseName]);
+		const newSuggestedCoursesNames = await this.generator.run([event.courseName]);
+		const updatedUser = user.updateSuggestedCourses(newSuggestedCoursesNames);
 
-		const updatedUser = user.updateSuggestedCourses(newSuggestedCourseName);
+		await this.createNewCourses(newSuggestedCoursesNames);
 
 		return this.repository.persist(updatedUser);
+	}
+
+	private async createNewCourses(courseNames: string[]): Promise<void[]> {
+		const createCoursePromises = courseNames.map(name => {
+			return this.courseCreator.run(UuidValueObject.random().value, name, '1 week');
+		});
+
+		return Promise.all(createCoursePromises);
 	}
 }
